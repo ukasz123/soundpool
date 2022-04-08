0a60846bc54e4cea4becce6dd598fd4b34f0c666
Antonio Calatrava <antonio@antoniocalatrava.com>
2022-04-08 10:10:03 +0200

    Remove AudioSession from implementation

--- soundpool/lib/src/platforms/ios.dart
+++ soundpool/lib/src/platforms/ios.dart
@@ -6,47 +6,11 @@ class SoundpoolOptionsIos {
   /// Default value: `true`
   final bool enableRate;
 
-  /// Audio Session category for shared [AVAudioSession](https://developer.apple.com/documentation/avfaudio/avaudiosession)
-  final AudioSessionCategory? audioSessionCategory;
-
-  /// Audio Session mode for shared [AVAudioSession](https://developer.apple.com/documentation/avfaudio/avaudiosession)
-  final AudioSessionMode audioSessionMode;
-
   const SoundpoolOptionsIos({
     this.enableRate = true,
-    this.audioSessionCategory,
-    this.audioSessionMode = AudioSessionMode.normal,
   });
 
   Map<String, dynamic> toOptionsMap() => {
         'enableRate': enableRate,
-        'avSessionCategory': audioSessionCategory?.toString().split('.').last,
-        'avSessionMode': audioSessionMode.toString().split('.').last,
       };
-}
-
-/// See:
-/// - [Audio Session Categories and Modes](https://developer.apple.com/library/archive/documentation/Audio/Conceptual/AudioSessionProgrammingGuide/AudioSessionCategoriesandModes/AudioSessionCategoriesandModes.html#//apple_ref/doc/uid/TP40007875-CH10)
-/// - [Audio Session Programming Guide](https://developer.apple.com/library/archive/documentation/Audio/Conceptual/AudioSessionProgrammingGuide/AudioSessionBasics/AudioSessionBasics.html#//apple_ref/doc/uid/TP40007875-CH3-SW1)
-enum AudioSessionCategory {
-  ambient,
-  soloAmbient,
-  playback,
-  playAndRecord,
-  multiRoute,
-}
-
-/// See:
-/// - [Audio Session Categories and Modes](https://developer.apple.com/library/archive/documentation/Audio/Conceptual/AudioSessionProgrammingGuide/AudioSessionCategoriesandModes/AudioSessionCategoriesandModes.html#//apple_ref/doc/uid/TP40007875-CH10)
-/// - [Audio Session Programming Guide](https://developer.apple.com/library/archive/documentation/Audio/Conceptual/AudioSessionProgrammingGuide/AudioSessionBasics/AudioSessionBasics.html#//apple_ref/doc/uid/TP40007875-CH3-SW1)
-enum AudioSessionMode {
-  /// 'Default' mode
-  normal,
-  moviePlayback,
-  videoRecording,
-  voiceChat,
-  gameChat,
-  videoChat,
-  spokenAudio,
-  measurement,
 }
